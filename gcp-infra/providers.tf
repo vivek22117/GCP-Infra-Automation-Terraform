1ab9e484e47092fc84c565c9e293eb7d24a4a972
vivek22117 <vivekmishra22117@gmail.com>
2021-07-10 10:09:59 +0530

    formatting TF scripts

--- gcp-infra/providers.tf
+++ gcp-infra/providers.tf
@@ -12,12 +12,12 @@ terraform {
 
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "~> 3.66"
     }
 
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.1"
     }
   }
