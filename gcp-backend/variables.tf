836aeccf26120585d3e0ee2946359430513e0d67
vivek22117 <vivekmishra22117@gmail.com>
2021-07-24 20:38:37 +0530

    adding common variables for GCP resource labels

--- gcp-backend/variables.tf
+++ gcp-backend/variables.tf
@@ -1,4 +1,4 @@
-#####======================== GCP authentication file ==========================#####
+#####======================== GCP authentication variables ==========================#####
 variable "gcp_auth_file" {
   type        = string
   description = "GCP authentication file"
@@ -12,4 +12,48 @@ variable "gcp_region" {
 variable "gcp_project" {
   type        = string
   description = "GCP project name"
+}
+
+#####===========================backend gcs configuration=================================#####
+variable "bucket_name_prefix" {
+  type        = string
+  description = "The prefix to the name of the Google Storage Bucket to create"
+}
+variable "storage_class" {
+  type        = string
+  description = "The storage class of the Storage Bucket to create"
+}
+
+
+######################################################
+# Local variables defined                            #
+######################################################
+variable "team" {
+  type        = string
+  description = "Owner team for this application infrastructure"
+}
+
+variable "owner" {
+  type        = string
+  description = "Owner of the product"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment to be used"
+}
+
+variable "component" {
+  type = string
+  description = "Project component for which resources are created"
+}
+
+#####=============Local variables===============#####
+locals {
+  common_tags = {
+    owner       = var.owner
+    team        = var.team
+    environment = var.environment
+    component = var.component
+  }
 }
