8e520b60986ac5a9384e1682dd87aafe9d96ac4b
vivek22117 <vivekmishra22117@gmail.com>
2021-07-25 15:14:42 +0530

    adding TF locals variables

---
+++ gcp-vpc-infra/locals.tf
@@ -0,0 +1,24 @@
+resource "random_integer" "suffix" {
+  max = 100
+  min = 1000000
+}
+
+locals {
+
+  region               = var.gcp_region
+  org_id               = var.org_id
+  folder_id            = var.folder_id
+  billing_account      = var.billing_account
+  host_project_name    = "network-host"
+  service_project_name = "application-project"
+  host_project_id      = "${local.host_project_name}-${random_integer.suffix.result}"
+  service_project_id   = "${local.service_project_name}-${random_integer.suffix.result}"
+
+  common_labels = {
+    owner       = var.owner
+    team        = var.team
+    environment = var.environment
+    component   = var.component
+  }
+
+}
