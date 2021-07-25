57b80982cc82909d0a4d6f5575d5dd1e6b6a9f81
vivek22117 <vivekmishra22117@gmail.com>
2021-07-25 19:45:51 +0530

    adding gcp projects

---
+++ gcp-vpc-infra/gcp-projects.tf
@@ -0,0 +1,31 @@
+resource "google_project" "network-host" {
+  name                = local.host_project_name
+  project_id          = local.host_project_id
+  billing_account     = local.billing_account
+  org_id              = local.org_id
+  folder_id           = local.folder_id
+  auto_create_network = false
+
+  labels = local.common_labels
+}
+
+resource "google_project" "application-host" {
+  name                = local.service_project_name
+  project_id          = local.service_project_id
+  billing_account     = local.billing_account
+  org_id              = local.org_id
+  folder_id           = local.folder_id
+  auto_create_network = false
+
+  labels = local.common_labels
+}
+
+//resource "google_project_service" "host" {
+//  project = google_project.host-staging.number
+//  service = local.projects_api
+//}
+//
+//resource "google_project_service" "service" {
+//  project = google_project.k8s-staging.number
+//  service = local.projects_api
+//}
